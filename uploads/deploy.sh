8d4aa72e232c3c4436b3983f79cf693fc9585218
liawne <liawne@users.noreply.github.com>
2024-01-09 00:20:36 +0000

    mv deploy.sh to archive 48cc39679912408c6e60636b43628a953c73ee29

---
+++ uploads/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+GIT_CURL_VERBOSE=1
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# delete old contents
+rm -rf public/*
+
+# Build the project.
+hugo --minify # if using a theme, replace with `Hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Published on $(date +'%Y-%m-%d %H:%M:%S')"
+if [ -n "$*" ]; then
+    msg="$*"
+fi
+
+# git clone git@github.com:liawne/liawne.github.io.git
+git commit -m "$msg"
+
+git remote -v 
+
+git pull --rebase origin master || exit 1
+#if ! timeout 5 9 git pull --rebase origin master; then
+#  git remote rm origin
+#  git remote add origin git@github.com:liawne/liawne.github.io.git
+#fi
+
+# Push source and build repos.
+git push -f origin master || exit 1
+
